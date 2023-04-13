47db3d982956672176cafd602733b8a7497f5687
Gabriel Francisco Oliveira <gfrancis@c1r8s5.42lisboa.com>
2023-04-13 16:01:41 +0100

    Commit 13 Apr, 16:01

---
+++ libft/libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gfrancis <gfrancis@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 14:42:03 by gfrancis          #+#    #+#             */
+/*   Updated: 2023/04/13 15:56:35 by gfrancis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+
+int	ft_isalpha(int c);
+size_t		ft_strlen(const char *s);
+int	ft_isascii(int c);
+int	ft_isdigit(int c);
+int	ft_isnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+
+#endif
