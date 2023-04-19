b2d76027efc2129554c564eb9792d1cd8a42791e
gfrancis <gfrancis@student.42lisboa.com>
2023-04-19 17:08:34 +0100

    Commit, 19 Apr, 17:08

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: gfrancis <gfrancis@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 14:42:03 by gfrancis          #+#    #+#             */
-/*   Updated: 2023/04/19 12:12:27 by gfrancis         ###   ########.fr       */
+/*   Updated: 2023/04/19 16:27:36 by gfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,8 +47,8 @@ char		*ft_strjoin(char const *s1, char const *s2);
 char		*ft_strtrim(char const *s1, char const *set);
 char		**ft_split(char const *s, char c);
 char		*ft_itoa(int n);
-//char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-//void		ft_striteri(char *s, void (*f)(unsigned int, char*));
+char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void		ft_striteri(char *s, void (*f)(unsigned int, char*));
 //void		ft_putchar_fd(char c, int fd);
 //void		ft_putstr_fd(char *s, int fd);
 //void		ft_putendl_fd(char *s, int fd);
