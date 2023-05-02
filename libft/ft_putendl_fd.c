e5b2d43460aab1309af22bd425644193fcac018c
gfrancis <gfrancis@student.42lisboa.com>
2023-05-02 16:25:17 +0100

    Commit, 2 May, 16:25

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: gfrancis <gfrancis@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 16:35:12 by gfrancis          #+#    #+#             */
-/*   Updated: 2023/05/02 11:13:59 by gfrancis         ###   ########.fr       */
+/*   Updated: 2023/05/02 11:38:18 by gfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 void	ft_putendl_fd(char *s, int fd)
 {
-	if (s == NULL || fd < 0)
-		return ;
 	ft_putstr_fd(s, fd);
 	ft_putchar_fd('\n', fd);
 }
